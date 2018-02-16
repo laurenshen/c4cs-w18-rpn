57dcdb9c51770d043546257e0e58957ab8cc0cd2
Lauren Shen <shenla@umich.edu>
2018-02-16 13:29:41 -0500

    revert because tests weren't running, build should fail here because added exponent test case but not in rpn

--- test_rpn.py
+++ test_rpn.py
@@ -1,7 +1,7 @@
 import unittest
 import rpn
 
-class TestBasics(unittest.TestCase):
+class TestStringMethods(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
@@ -17,3 +17,6 @@ class TestBasics(unittest.TestCase):
     def test_exponent(self):
         result = rpn.calculate("2 0 ^")
         self.assertEqual(1, result)
+
+if __name__ == '__main__':
+    unittest.main()
